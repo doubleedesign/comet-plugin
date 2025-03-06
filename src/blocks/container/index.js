42f3332e154e3cd12108bb8855d0c78f7a74f7e6
Leesa Ward <leesa@doubleedesign.com.au>
2025-03-06 22:44:22 +1100

    Refactor(WP container): Use proper attribute instead of style for container size

--- src/blocks/container/index.js
+++ src/blocks/container/index.js
@@ -2,27 +2,63 @@
 
 wp.domReady(() => {
 	const { registerBlockType } = wp.blocks;
-	const { useBlockProps, InnerBlocks } = wp.blockEditor;
+	const { InspectorControls, useBlockProps, InnerBlocks } = wp.blockEditor;
 	const { createElement } = wp.element;
+	const { PanelBody, SelectControl } = wp.components;
 
 	registerBlockType('comet/container', {
-		edit: () => {
-			const blockProps = useBlockProps({
-				className: 'section'
-			});
+		edit: ({ attributes, setAttributes }) => {
+
+			const containerSizeControl = createElement(
+				PanelBody,
+				{ title: 'Layout' },
+				createElement(
+					SelectControl,
+					{
+						label: 'Container size',
+						value: attributes.size,
+						options: [
+							{ label: 'Default', value: 'default' },
+							{ label: 'Narrow', value: 'narrow' },
+							{ label: 'Wide', value: 'wide' },
+							{ label: 'Full-width', value: 'fullwidth' }
+						],
+						onChange: (newValue) => setAttributes({ size: newValue }),
+						// eslint-disable-next-line max-len
+						help: 'Note: Some page templates (such as those with a sidebar) have their own hard-coded container(s) and may ignore containers. The Group block may be more suitable for these cases.'
+					}
+				));
+
+
+			const blockProps = useBlockProps();
 			const template = [
 				['core/paragraph']
 			];
 
-			return createElement('section',
+			return createElement(
+				'div',
 				blockProps,
+				// Block editor sidebar
 				createElement(
-					'div',
-					{ className: 'container' },
-					createElement(InnerBlocks, {
-						template: template
-					})
+					InspectorControls,
+					null,
+					containerSizeControl,
 				),
+				// Block preview
+				createElement('section',
+					blockProps,
+					createElement(
+						'div',
+						{ className: 'container' },
+						createElement(
+							'div',
+							{ 'data-size': attributes.size },
+							createElement(InnerBlocks, {
+								template: template
+							})
+						)
+					)
+				)
 			);
 		},
 		save: () => {
