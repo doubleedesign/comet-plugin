8664e929831ed3095cfb9634e5803a95ba1176c0
Leesa Ward <leesa@doubleedesign.com.au>
2025-04-10 16:17:11 +1000

    Refactor(Accordion,Tabs,ResponsivePanels): Simplify contents of the various Panels

--- src/blocks/panel/index.js
+++ src/blocks/panel/index.js
@@ -2,39 +2,124 @@
 
 wp.domReady(() => {
 	const { registerBlockType } = wp.blocks;
-	const { useBlockProps, InnerBlocks } = wp.blockEditor;
-	const { createElement } = wp.element;
+	const { useBlockProps, InnerBlocks, RichText } = wp.blockEditor;
+	const { createElement, Fragment } = wp.element;
 
 	registerBlockType('comet/panel', {
-		edit: ({ context }) => {
+		attributes: {
+			title: {
+				type: 'string',
+				default: ''
+			},
+			subtitle: {
+				type: 'string',
+				default: ''
+			}
+		},
+		edit: ({ attributes, setAttributes, context }) => {
+			const { title, subtitle } = attributes;
 			const variant = context['comet/variant'];
 			const blockProps = useBlockProps({
 				className: `${variant}__panel`,
 				open: true
 			});
+
+			// Define allowed blocks directly in the panel
+			const allowedBlocks = [
+				'core/group',
+				'core/columns',
+				'core/heading',
+				'core/paragraph',
+				'core/list',
+				'core/image',
+				'core/file'
+			];
+
+			// Set up a template with a paragraph block
 			const template = [
-				['comet/panel-title', {
-					'placeholder': 'Add panel title here...',
-					'lock': { 'remove': true }
-				}],
-				['comet/panel-content', {
-					'lock': { 'remove': true }
+				['core/paragraph', {
+					'placeholder': 'Add panel content here...'
 				}]
 			];
 
-			return createElement('div',
+			return createElement(
+				'div',
 				blockProps,
-				createElement(InnerBlocks, {
-					allowedBlocks: ['comet/panel-title', 'comet/panel-content'],
-					template: template,
-					templateLock: 'insert'
-				})
+				createElement(
+					Fragment,
+					null,
+					// Title section
+					createElement(
+						RichText,
+						{
+							tagName: 'span',
+							className: `${variant}__panel-title`,
+							value: title,
+							onChange: (value) => setAttributes({ title: value }),
+							placeholder: 'Add panel title here...'
+						}
+					),
+					// Subtitle section
+					createElement(
+						RichText,
+						{
+							tagName: 'small',
+							className: `${variant}__panel-subtitle`,
+							value: subtitle,
+							onChange: (value) => setAttributes({ subtitle: value }),
+							placeholder: 'Add panel subtitle here...'
+						}
+					),
+					// Content section
+					createElement(
+						'div',
+						{ className: `${variant}__panel__content` },
+						createElement(
+							InnerBlocks,
+							{
+								allowedBlocks: allowedBlocks,
+								template: template,
+								templateLock: false
+							}
+						)
+					)
+				)
 			);
 		},
-		save: () => {
-			return createElement('div',
+		save: ({ attributes }) => {
+			const { title, subtitle } = attributes;
+
+			return createElement(
+				'div',
 				useBlockProps.save(),
-				createElement(InnerBlocks.Content)
+				createElement(
+					Fragment,
+					null,
+					// Title section
+					createElement(
+						RichText.Content,
+						{
+							tagName: 'span',
+							className: 'panel-title',
+							value: title
+						}
+					),
+					// Subtitle section
+					createElement(
+						RichText.Content,
+						{
+							tagName: 'small',
+							className: 'panel-subtitle',
+							value: subtitle
+						}
+					),
+					// Content section
+					createElement(
+						'div',
+						{ className: 'panel__content' },
+						createElement(InnerBlocks.Content)
+					)
+				)
 			);
 		}
 	});
