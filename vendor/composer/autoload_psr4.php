ea4331a44592f357cbff53dbb53499f9bce908e8
Leesa Ward <leesa@doubleedesign.com.au>
2025-08-27 13:01:20 +1000

    Chore: Config and dependency updates

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -26,7 +26,8 @@ return array(
     'Illuminate\\Container\\' => array($vendorDir . '/illuminate/container'),
     'Illuminate\\Bus\\' => array($vendorDir . '/illuminate/bus'),
     'Doubleedesign\\Comet\\WordPress\\' => array($baseDir . '/src'),
-    'Doctrine\\Inflector\\' => array($vendorDir . '/doctrine/inflector/lib/Doctrine/Inflector'),
+    'Doubleedesign\\Comet\\Core\\' => array($vendorDir . '/doubleedesign/comet-components-core/src/base/types', $vendorDir . '/doubleedesign/comet-components-core/src/base/attributes', $vendorDir . '/doubleedesign/comet-components-core/src/base/traits', $vendorDir . '/doubleedesign/comet-components-core/src/base/annotations', $vendorDir . '/doubleedesign/comet-components-core/src/base/components', $vendorDir . '/doubleedesign/comet-components-core/src/base', $vendorDir . '/doubleedesign/comet-components-core/src/services'),
+    'Doctrine\\Inflector\\' => array($vendorDir . '/doctrine/inflector/src'),
     'Carbon\\Doctrine\\' => array($vendorDir . '/carbonphp/carbon-doctrine-types/src/Carbon/Doctrine'),
     'Carbon\\' => array($vendorDir . '/nesbot/carbon/src/Carbon'),
 );
